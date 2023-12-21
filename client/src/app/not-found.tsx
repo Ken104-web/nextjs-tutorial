4d77b96c14722c23f746ccf2604ec833ca01e0b0
KenCorp <kennethwarui35@gmail.com>
2023-12-21 13:32:02 +0300

    Colocation and private folders

---
+++ client/src/app/not-found.tsx
@@ -0,0 +1,10 @@
+export default function NotFound(){
+    return (
+        <div>
+            <h2>
+                Page Not Found
+            </h2>
+            <p>Sorry Bro but could not find requested Resource</p>
+        </div>
+    );
+}
--- client/src/app/product/[productId]/reviews/[reviewId]/page.tsx
+++ client/src/app/product/[productId]/reviews/[reviewId]/page.tsx
@@ -1,6 +1,10 @@
+import { notFound } from "next/navigation";
 export default function ReviewDetails({params} : {
     params : {productId: string; reviewId: string;}
 }){
+    if(parseInt(params.reviewId) > 1000){
+        notFound();
+    }
     return (
         <div>
             <h1>Review details {params.reviewId} for product {params.productId}</h1>
